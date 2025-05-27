6cace166f6e4e24e75951a0a3e3a38df543bb094
Nicola De Cristofaro <nicoladecristofaro.dev@gmail.com>
2025-05-27 23:09:28 +0200

    fix: kms policy variable

--- terraform/modules/kms/main.tf
+++ terraform/modules/kms/main.tf
@@ -2,6 +2,7 @@ resource "aws_kms_key" "kms" {
   deletion_window_in_days  = var.kms_deletion_window_in_days
   enable_key_rotation      = var.kms_enable_key_rotation
   description              = var.kms_description
+  policy                   = var.kms_policy
   key_usage                = var.kms_key_usage
   customer_master_key_spec = var.kms_customer_master_key_spec
   multi_region             = var.kms_multi_region
